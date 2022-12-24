34219d3cd22f3f8f266b15fbdac5d718bb2dc853
NipunEranda <namarasekara71@gmail.com>
2022-12-24 19:51:39 +0530

    initial files added

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue';
+import Index from '../views/Index.vue';
+import store from '../store';
+
+const routes = [
+  {
+    path: '/home',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/',
+    name: 'index',
+    component: Index
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+router.beforeEach((to, from) => {
+  if(!store.state.auth.currentUser && to.name !== "index")
+    return '/';
+});
+
+export default router;
